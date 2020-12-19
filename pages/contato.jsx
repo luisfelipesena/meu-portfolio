cddfbdcad0de57da011c6f8ce0c247d72a5dbd6b
Luis Felipe <luisfelipesena@gmail.com>
2020-12-19 17:46:50 -0300

    feat: starting pages in next and typescript react

---
+++ pages/contato.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Contato() {
+  return (
+    <>
+      <div>OI</div>
+      <Link href="/">Home</Link>
+      <Link href="/teste/ele">Pessoa</Link>
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <div>OI</div>
+      <Link href="/contato">Contato</Link>
+      <Link href="/teste/jonathas">Pessoa</Link>
+    </>
+  );
+}
