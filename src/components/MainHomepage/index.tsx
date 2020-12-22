943102fdff8664ea44fe8ebefe63624644c22042
Luis Felipe <luisfelipesena@gmail.com>
2020-12-22 20:44:09 -0300

    feat: serverless inittial

--- src/components/MainHomepage/index.tsx
+++ src/components/MainHomepage/index.tsx
@@ -1,10 +1,38 @@
 import { Main } from "./style";
+import useSWR from "swr";
+import { UseFetch } from "../../helpers/UseFetch";
 
-export const MainComponent = ({ data }) => {
+interface iProps {
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+}
+
+const thisFetcher = (url: string) =>
+  UseFetch(url, "GET", undefined).then((res) => res.json());
+
+export const MainComponent = () => {
+  const { data, error } = useSWR("/api/getCity", thisFetcher);
+  if (!data) {
+    return (
+      <Main>
+        <div className="container">Skeleton Antd</div>
+      </Main>
+    );
+  }
+  if (error) {
+    <Main>
+      <div className="container">A Página deu um probleminha !!!</div>
+    </Main>;
+  }
+
+  const response: iProps = data?.data;
   return (
     <Main>
       <div className="container">
-        Você está vindo da {data.regionName} ? Que Bom !!!
+        Você está vindo da {response.regionName} ? Que Bom !!!
       </div>
     </Main>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,15 @@
 import Head from "next/head";
-import { GetStaticProps } from "next";
 import { HeaderComponent } from "../components/Header";
 import { MainComponent } from "../components/MainHomepage";
 
-const Home = ({ data }) => {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Meu Portifólio</title>
       </Head>
       <HeaderComponent />
-      <MainComponent data={data} />
+      <MainComponent />
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://ip-api.com/json");
-  const data = await response.json();
-  return {
-    props: {
-      data,
-    },
-  };
-};
-
-export default Home;
+}
