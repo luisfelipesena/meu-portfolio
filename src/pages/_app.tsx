d040682c6c21c1f3170ba8f85dac36eaf65e5107
luisfelipesena <luis.sena@cubos.io>
2021-06-01 10:33:46 -0300

    feat: localStorage-theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import Head from "next/head";
@@ -9,7 +9,25 @@ import LightTheme, { Darktheme } from "@styles/themes";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [choosedTheme, setChoosedTheme] = useState(Darktheme);
+
+  if (typeof window !== "undefined") {
+    const storage = localStorage.getItem("theme");
+    useEffect(() => {
+      localStorage.setItem(
+        "theme",
+        storage ? JSON.stringify(storage) : JSON.stringify(Darktheme)
+      );
+      setChoosedTheme(JSON.parse(storage) ?? Darktheme);
+    }, []);
+  }
+
   const toggleTheme = () => {
+    localStorage.setItem(
+      "theme",
+      choosedTheme.title === LightTheme.title
+        ? JSON.stringify(Darktheme)
+        : JSON.stringify(LightTheme)
+    );
     setChoosedTheme(
       choosedTheme.title === LightTheme.title ? Darktheme : LightTheme
     );
